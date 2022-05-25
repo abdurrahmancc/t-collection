e16ef583f61bc2a1ad86fbef563e46a2d311a62e
abdurrahmancc <personalbusinessmail63@gmail.com>
2022-05-25 23:39:56 +0600

    stripe payment method add

---
+++ src/Components/Hooks/CheckoutForm.js
@@ -0,0 +1,67 @@
+import { CardElement, ElementsConsumer, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [cardError, setCardError] = useState("");
+
+  const handleSubmit = async (event) => {
+    // Block native form submission.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    // Use your card Element with other Stripe.js APIs
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      setCardError(error?.message);
+      console.log(error?.message);
+    } else {
+      setCardError(" ");
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button type="submit" disabled={!stripe} className={" btn btn-sm btn-primary mt-10"}>
+          Pay
+        </button>
+      </form>
+      {cardError && <span className="label-text-alt text-red-500">{cardError}</span>}
+    </>
+  );
+};
+
+export default CheckoutForm;
--- src/Components/Hooks/Payment.js
+++ src/Components/Hooks/Payment.js
@@ -1,21 +1,37 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
 
 const Payment = () => {
   return (
     <div>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
+      <h4>please pay for {}</h4>
+      <div class="hero min-h-fit mt-12 bg-base-200">
+        <div class="hero-content flex-col lg:flex-col">
+          <div class="text-center lg:text-left">
+            <div class="card w-96 bg-base-100 shadow-xl">
+              <div class="card-body">
+                <h2 class="card-title">Hello, {}</h2>
+                <p>Please Pay for {}</p>
+                <p>
+                  Your Appointment <span className="text-orange-700">{}</span>
+                </p>
+                <p>Please Pay ${}</p>
+              </div>
+            </div>
+          </div>
+          <div class="card max-h-52 flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div class="card-body">
+              <Elements stripe={stripePromise}>
+                <CheckoutForm />
+              </Elements>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
